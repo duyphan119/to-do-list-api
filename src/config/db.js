4c99aedad8d703c463e821a9d9d42e41ce0ccc11
duyphan119 <duy.910.2015@gmail.com>
2022-02-17 15:16:07 +0700

    change db

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 const connectDatabase = () => {
   try {
-    mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`);
+    mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.comsr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    );
     console.log("Database is connected");
   } catch (error) {
     console.log(error);
   }
 };
+
 module.exports = connectDatabase;
